b3982d7cffc283b42668cf11f0f921e406d7cc93
Brijesh <brijeshburfal18@gmail.com>
2020-10-28 14:17:29 +0530

    Updates

---
+++ precache-manifest.ab2ff0e6ab528df744d8e811129bf8c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239ba77478e30a1ae627fcbc04b73996",
+    "url": "/React-SeasonApp/index.html"
+  },
+  {
+    "revision": "70941645a1c1f19b76c1",
+    "url": "/React-SeasonApp/static/css/main.2c03e86c.chunk.css"
+  },
+  {
+    "revision": "a9fd37878cd2a9f15154",
+    "url": "/React-SeasonApp/static/js/2.698247c5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-SeasonApp/static/js/2.698247c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70941645a1c1f19b76c1",
+    "url": "/React-SeasonApp/static/js/main.7c22ab5d.chunk.js"
+  },
+  {
+    "revision": "1bfa55fcd586f623d2b0",
+    "url": "/React-SeasonApp/static/js/runtime-main.ebc68951.js"
+  }
+]);
